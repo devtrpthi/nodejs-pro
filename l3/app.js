6b4ccc352529be90d12f03b24f5fe78387ca7b6f
devesh <deveshkrishnatripathi@gmail.com>
2022-10-07 16:54:28 +0530

    app.js greeting added

--- l3/app.js
+++ l3/app.js
@@ -1,3 +1,22 @@
 const express = require('express');
 
+const greetMiddleware  = require('./greet.js');
 
+class greetingService {
+  constructor(greeting = 'Hello') {
+    this.greeting = greeting;
+  }
+
+  createGreeting(name){
+    return `$(this.greeting), $(name)!`;
+  }
+}
+
+express()
+.use('/api/vi/service1', greetMiddleware({
+    service: new greetingService('Hello'),
+  }))
+.use('/api/v1/service2', greetMiddleware({ 
+    service: new greetingService('Hi'),
+  }))
+.listen(3000);
