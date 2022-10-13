e48584540c9b7428209f54fe19ebac25e17a7de1
devesh <deveshkrishnatripathi@gmail.com>
2022-10-13 18:12:08 +0530

    response added

--- api-tut/server.js
+++ api-tut/server.js
@@ -15,9 +15,9 @@ app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req,res) => {
   res.json({message: 'Welcome to my application'});
-})
+});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT,() => {
   console.log(`Server is running on port ${PORT}`)
-})
+});
