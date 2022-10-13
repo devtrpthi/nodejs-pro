194dc3df20e44b9f5e746a27fb359575b583e534
devesh <deveshkrishnatripathi@gmail.com>
2022-10-13 20:47:18 +0530

    modified

--- api-tut/server.js
+++ api-tut/server.js
@@ -13,6 +13,21 @@ app.use(express.json());
 
 app.use(express.urlencoded({extended: true}));
 
+const db = require('./app/models');
+
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('Connected to database');
+  })
+  .catch(err => {
+    console.log('Cannot connect to the database', err);
+    process.exit();
+  });
+
 app.get('/', (req,res) => {
   res.json({message: 'Welcome to my application'});
 });
