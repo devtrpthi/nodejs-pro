bff4f3ac76f1540b1c99bd707fe782963738b1c0
devesh <deveshkrishnatripathi@gmail.com>
2022-10-14 16:43:21 +0530

    modified

--- api-tut/app/controllers/tutorial.controller.js
+++ api-tut/app/controllers/tutorial.controller.js
@@ -50,7 +50,27 @@ exports.findOne = (req,res) => {
 };
 
 exports.update = (req,res) => {
+  if (!req.body) {
+    return res.status(400).send({
+      message: "Data to update can not be empty!"
+    });
+  }
 
+  const id = req.params.id;
+
+  Tutorial.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found!`
+        });
+      } else res.send({ message: "Tutorial was updated successfully." });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating Tutorial with id=" + id
+      });
+    }); 
 };
 
 exports.delete = (req,res) => {
